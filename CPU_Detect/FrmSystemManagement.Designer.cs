b894ac5f8c0fb3e4f0d0f8c323c3cecdb840e8f1
13240137763@163.com <qiyubrother>
2019-12-26 13:36:02 +0800

    .

---
+++ CPU_Detect/FrmSystemManagement.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace CPU_Detect
+{
+    partial class FrmSystemManagement
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmSystemManagement));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lblCPU = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblMuse = new System.Windows.Forms.Label();
+            this.lblSum = new System.Windows.Forms.Label();
+            this.pbMemoryUse = new System.Windows.Forms.ProgressBar();
+            this.pbMemorySum = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Location = new System.Drawing.Point(8, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(89, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CPU 使用";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.lblCPU);
+            this.panel1.Location = new System.Drawing.Point(7, 16);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(74, 79);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Green;
+            this.panel3.Location = new System.Drawing.Point(26, 9);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(22, 48);
+            this.panel3.TabIndex = 4;
+            // 
+            // lblCPU
+            // 
+            this.lblCPU.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblCPU.AutoSize = true;
+            this.lblCPU.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblCPU.ForeColor = System.Drawing.Color.Lime;
+            this.lblCPU.Location = new System.Drawing.Point(24, 66);
+            this.lblCPU.Name = "lblCPU";
+            this.lblCPU.Size = new System.Drawing.Size(0, 12);
+            this.lblCPU.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblMuse);
+            this.groupBox2.Controls.Add(this.lblSum);
+            this.groupBox2.Controls.Add(this.pbMemoryUse);
+            this.groupBox2.Controls.Add(this.pbMemorySum);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(103, 7);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(321, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "物理内存(MiB)";
+            // 
+            // lblMuse
+            // 
+            this.lblMuse.Location = new System.Drawing.Point(248, 25);
+            this.lblMuse.Name = "lblMuse";
+            this.lblMuse.Size = new System.Drawing.Size(67, 12);
+            this.lblMuse.TabIndex = 7;
+            // 
+            // lblSum
+            // 
+            this.lblSum.Location = new System.Drawing.Point(248, 44);
+            this.lblSum.Name = "lblSum";
+            this.lblSum.Size = new System.Drawing.Size(67, 12);
+            this.lblSum.TabIndex = 6;
+            // 
+            // pbMemoryUse
+            // 
+            this.pbMemoryUse.Location = new System.Drawing.Point(60, 25);
+            this.pbMemoryUse.Name = "pbMemoryUse";
+            this.pbMemoryUse.Size = new System.Drawing.Size(183, 13);
+            this.pbMemoryUse.TabIndex = 4;
+            // 
+            // pbMemorySum
+            // 
+            this.pbMemorySum.Location = new System.Drawing.Point(60, 44);
+            this.pbMemorySum.Name = "pbMemorySum";
+            this.pbMemorySum.Size = new System.Drawing.Size(183, 13);
+            this.pbMemorySum.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "可用数：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "总数：";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // FrmSystemManagement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 114);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmSystemManagement";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CPU使用率";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblCPU;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar pbMemoryUse;
+        private System.Windows.Forms.ProgressBar pbMemorySum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblMuse;
+        private System.Windows.Forms.Label lblSum;
+
+    }
+}
+
--- CPU_Detect/FrmSystemManagement.cs
+++ CPU_Detect/FrmSystemManagement.cs
@@ -12,9 +12,9 @@ using System.Runtime.InteropServices;
 using System.Management;
 namespace CPU_Detect
 {
-    public partial class Form1 : Form
+    public partial class FrmSystemManagement : Form
     {
-        public Form1()
+        public FrmSystemManagement()
         {
             InitializeComponent();
         }
@@ -23,50 +23,47 @@ namespace CPU_Detect
         private void myUser()
         {
             ManagementObjectSearcher searcher = new ManagementObjectSearcher("select * from Win32_Processor");
+            int iCPU = 0;
+            int calcCPU = 0;
+            int perCPU = 0;
             foreach (ManagementObject myobject in searcher.Get())
             {
-                tssluse.Text = myobject["LoadPercentage"].ToString()+" %";
-                lblCPU.Text = myobject["LoadPercentage"].ToString() + " %";
-                mheight = Convert.ToInt32(myobject["LoadPercentage"].ToString());
-                if (mheight == 100)
-                    panel3.Height = 100;
-                CreateImage();
-                Memory();
+
+                iCPU++;
+                calcCPU = Convert.ToInt32(myobject["LoadPercentage"]);
+                perCPU += calcCPU;
             }
+            perCPU = perCPU / iCPU;
+            lblCPU.Text = perCPU + " %";
+            mheight = Convert.ToInt32(perCPU);
+            if (mheight == 100)
+                panel3.Height = 100;
+            CreateImage();
+            Memory();
         }
 
         private void Memory()
         {
             Microsoft.VisualBasic.Devices.Computer myInfo = new Microsoft.VisualBasic.Devices.Computer();
             //获取物理内存总量
-            pbMemorySum.Maximum = Convert.ToInt32(myInfo.Info.TotalPhysicalMemory/1024/1024);
+            pbMemorySum.Maximum = Convert.ToInt32(myInfo.Info.TotalPhysicalMemory /1024 / 1024);
             pbMemorySum.Value = Convert.ToInt32(myInfo.Info.TotalPhysicalMemory / 1024 / 1024);
-            lblSum.Text = (myInfo.Info.TotalPhysicalMemory / 1024).ToString();
+            lblSum.Text = (myInfo.Info.TotalPhysicalMemory / 1024 / 1024).ToString("###,###");
             //获取可用物理内存总量
             pbMemoryUse.Maximum = Convert.ToInt32(myInfo.Info.TotalPhysicalMemory / 1024 / 1024);
             pbMemoryUse.Value = Convert.ToInt32(myInfo.Info.AvailablePhysicalMemory / 1024 / 1024);
-            lblMuse.Text = (myInfo.Info.AvailablePhysicalMemory / 1024).ToString();
-            //获取虚拟内存总量
-            pbVmemorysum.Maximum = Convert.ToInt32(myInfo.Info.TotalVirtualMemory / 1024 / 1024);
-            pbVmemorysum.Value = Convert.ToInt32(myInfo.Info.TotalVirtualMemory / 1024 / 1024);
-            lblVinfo.Text = (myInfo.Info.TotalVirtualMemory / 1024).ToString();
-            //获取可用虚拟内存总量
-            pbVmemoryuse.Maximum = Convert.ToInt32(myInfo.Info.TotalVirtualMemory / 1024 / 1024);
-            pbVmemoryuse.Value = Convert.ToInt32(myInfo.Info.AvailableVirtualMemory/ 1024 / 1024);
-            lblVuse.Text = (myInfo.Info.AvailableVirtualMemory / 1024).ToString();
+            lblMuse.Text = (myInfo.Info.AvailablePhysicalMemory / 1024 / 1024).ToString("###,###");
         }
         private void Form1_Load(object sender, EventArgs e)
         {
             CheckForIllegalCrossThreadCalls = false;
             MyProcesses = Process.GetProcesses();
-            tsslNum.Text = MyProcesses.Length.ToString();
             myUser();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             MyProcesses = Process.GetProcesses();
-            tsslNum.Text = MyProcesses.Length.ToString();
             td = new Thread(new ThreadStart(myUser));
             td.Start();
         }
@@ -81,15 +78,13 @@ namespace CPU_Detect
         int mheight = 0;
         private void CreateImage()
         {
-            int i=panel3.Height/100;
             Bitmap image = new Bitmap(panel3.Width,panel3.Height);
             //创建Graphics类对象
             Graphics g = Graphics.FromImage(image);
             g.Clear(Color.Green);
-            SolidBrush mybrush = new SolidBrush(Color.Lime);
-            g.FillRectangle(mybrush,0,panel3.Height-mheight*i,26,mheight*i);
+            SolidBrush mybrush = new SolidBrush(Color.Red);
+            g.FillRectangle(mybrush,0,panel3.Height*(1- mheight * 1.0f / 100),26, panel3.Height * (mheight * 1.0f / 100));
             panel3.BackgroundImage = image;
-
         }
     }
 }
